91229da71b77bcd01b2e6167f43fb5577f903a3e
Long Pham <philong.bmt@gmail.com>
2023-10-30 16:08:14 +0700

    init project

---
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+struct MyLibrary {
+    func sayHello() -> String {
+        print("Hello World.")
+        
+        return "Hello"
+    }
+}
